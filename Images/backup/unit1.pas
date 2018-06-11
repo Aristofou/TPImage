d919271bce9e5f2014d65550cfca394bb1d06131
DESKTOP-C15LQ6Q\nonoa <a.acker@ludus-academie.com>
2018-06-11 17:44:58 +0200

    Voici les deux tp images, avec l'image car je ne savais pas si il fallait l'envoyer

---
+++ Images/backup/unit1.pas
@@ -0,0 +1,150 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    RaZ: TButton;
+    ButtonEch: TButton;
+    ButtonLignes: TButton;
+    Button4c: TButton;
+    ButtonN: TButton;
+    ButtonR: TButton;
+    Image1: TImage;
+    procedure RaZClick(Sender: TObject);
+    procedure Button4cClick(Sender: TObject);
+    procedure ButtonEchClick(Sender: TObject);
+    procedure ButtonLignesClick(Sender: TObject);
+    procedure ButtonNClick(Sender: TObject);
+    procedure ButtonRClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+var
+  Form1: TForm1;
+  i,f,g:integer;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.ButtonNClick(Sender: TObject);
+begin
+  Image1.canvas.brush.color:= clBlack;
+  Image1.canvas.Rectangle(1,1,400,400);
+end;
+
+procedure TForm1.Button4cClick(Sender: TObject);
+begin
+  Image1.canvas.brush.color:= clPurple;
+  Image1.canvas.Rectangle(1,1,200,200);
+  Image1.canvas.brush.color:= clGreen;
+  Image1.canvas.Rectangle(200,1,400,200);
+  Image1.canvas.brush.color:= clBlue;
+  Image1.canvas.Rectangle(200,200,1,400);
+  Image1.canvas.brush.color:= clYellow;
+  Image1.canvas.Rectangle(200,200,400,400);
+end;
+
+procedure TForm1.RaZClick(Sender: TObject);
+begin
+    Image1.Canvas.Clear;
+end;
+
+procedure TForm1.ButtonEchClick(Sender: TObject);
+begin
+  i:=0;
+  for g:=1 to 8 do
+    begin
+    Image1.canvas.brush.color:= clBlack;
+    Image1.canvas.Rectangle(1+i,1+i,50+i,50+i);
+    Image1.canvas.Brush.color:= clWhite;
+    Image1.canvas.Rectangle(50+i,1+i,100+i,50+i);
+    Image1.canvas.brush.color:= clBlack;
+    Image1.canvas.Rectangle(100+i,1+i,150+i,50+i);
+    Image1.canvas.Brush.color:= clWhite;
+    Image1.canvas.Rectangle(150+i,1+i,200+i,50+i);
+    Image1.canvas.brush.color:= clBlack;
+    Image1.canvas.Rectangle(200+i,1+i,250+i,50+i);
+    Image1.canvas.Brush.color:= clWhite;
+    Image1.canvas.Rectangle(250+i,1+i,300+i,50+i);
+    Image1.canvas.brush.color:= clBlack;
+    Image1.canvas.Rectangle(300+i,1+i,350+i,50+i);
+    Image1.canvas.Brush.color:= clWhite;
+    Image1.canvas.Rectangle(350+i,1+i,400+i,50+i);
+    Image1.canvas.brush.color:= clBlack;
+    Image1.canvas.Rectangle(1-i,1+i,50-i,50+i);
+    Image1.canvas.Brush.color:= clWhite;
+    Image1.canvas.Rectangle(50-i,1+i,100-i,50+i);
+    Image1.canvas.brush.color:= clBlack;
+    Image1.canvas.Rectangle(100-i,1+i,150-i,50+i);
+    Image1.canvas.Brush.color:= clWhite;
+    Image1.canvas.Rectangle(150-i,1+i,200-i,50+i);
+    Image1.canvas.brush.color:= clBlack;
+    Image1.canvas.Rectangle(200-i,1+i,250-i,50+i);
+    Image1.canvas.Brush.color:= clWhite;
+    Image1.canvas.Rectangle(250-i,1+i,300-i,50+i);
+    Image1.canvas.brush.color:= clBlack;
+    Image1.canvas.Rectangle(300-i,1+i,350-i,50+i);
+    Image1.canvas.Brush.color:= clWhite;
+    Image1.canvas.Rectangle(350-i,1+i,400-i,50+i);
+    Image1.canvas.Rectangle(100,350,150,400);
+    Image1.canvas.Rectangle(150,300,200,350);
+    Image1.canvas.Rectangle(200,250,250,300);
+    Image1.canvas.Rectangle(200,350,250,400);
+    Image1.canvas.Rectangle(250,300,300,350);
+    Image1.canvas.Rectangle(300,350,350,400);
+    Image1.canvas.brush.color:= clBlack;
+    Image1.canvas.Rectangle(50,350,100,400);
+    Image1.canvas.Rectangle(100,300,150,350);
+    Image1.canvas.Rectangle(150,250,200,300);
+    Image1.canvas.Rectangle(150,350,200,400);
+    Image1.canvas.Rectangle(200,300,250,350);
+    Image1.canvas.Rectangle(250,350,300,400);
+
+    i:=i+50;
+    end;
+end;
+
+procedure TForm1.ButtonLignesClick(Sender: TObject);
+begin
+
+  for f:=1 to 400 do
+    begin
+    Image1.canvas.pen.color:= clPurple;
+    Image1.canvas.Line(1+i,1,1+i,400);
+    i:=i+1 ;
+    Image1.canvas.pen.color:= clGreen;
+    Image1.canvas.Line(1+i,1,1+i,400);
+    i:=i+1;
+    Image1.canvas.pen.color:= clBlue;
+    Image1.canvas.Line(1+i,1,1+i,400);
+    i:=i+1;
+    Image1.canvas.pen.color:= clRed;
+    Image1.canvas.Line(1+i,1,1+i,400);
+    i:=i+1;
+    end;
+  f:=1;
+end;
+
+procedure TForm1.ButtonRClick(Sender: TObject);
+begin
+  Image1.canvas.Brush.color:= clRed;
+  Image1.canvas.Rectangle(1,1,400,400);
+end;
+
+end.
+
---
+++ Images_Symétrie/unit1.pas
@@ -0,0 +1,137 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ButtonDoubleM: TButton;
+    ButtonMirrorH: TButton;
+    ButtonMirrorV: TButton;
+    ButtonHoriz: TButton;
+    ButtonVert: TButton;
+    Image1: TImage;
+    Image2: TImage;
+    procedure ButtonDoubleMClick(Sender: TObject);
+    procedure ButtonHorizClick(Sender: TObject);
+    procedure ButtonMirrorHClick(Sender: TObject);
+    procedure ButtonMirrorVClick(Sender: TObject);
+    procedure ButtonVertClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  i,j:integer;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.ButtonVertClick(Sender: TObject);
+begin
+   for i:=1 to 170 do
+       begin
+       for j:=1 to 170 do
+           begin
+           Image2.canvas.pixels[i,j]:=Image1.canvas.pixels[170-i,j];
+           end;
+
+       end;
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.ButtonHorizClick(Sender: TObject);
+begin
+  for i:=1 to 170 do
+       begin
+       for j:=1 to 170 do
+           begin
+           Image2.canvas.pixels[i,j]:=Image1.canvas.pixels[170-i,170-j];
+           end;
+
+       end;
+end;
+
+procedure TForm1.ButtonDoubleMClick(Sender: TObject);
+begin
+     for j:=1 to 85 do
+      begin
+      for i:=1 to 170 do
+          begin
+          Image2.Canvas.pixels[i,j]:=Image1.Canvas.Pixels[i,j]
+          end;
+      end;
+      for i:=86 to 170 do
+        begin
+        for j:=1 to 170 do
+          begin
+            Image2.canvas.pixels[i,j]:=Image1.canvas.pixels[170-i,170-j];
+          end;
+        end;
+      for i:=1 to 85 do
+        begin
+             for j:=1 to 85 do
+               begin
+                 Image2.Canvas.pixels[i,j]:=Image1.Canvas.Pixels[i,j];
+               end;
+        end;
+end;
+
+procedure TForm1.ButtonMirrorHClick(Sender: TObject);
+begin
+  for j:=1 to 85 do
+      begin
+      for i:=1 to 170 do
+          begin
+          Image2.Canvas.pixels[i,j]:=Image1.Canvas.Pixels[i,j]
+          end;
+      end;
+  for j:=86 to 170 do
+      begin
+      for i:=1 to 170 do
+          begin
+               Image2.canvas.pixels[i,j]:=Image1.canvas.pixels[i,170-j]
+          end;
+      end;
+end;
+
+procedure TForm1.ButtonMirrorVClick(Sender: TObject);
+begin
+      for i:=1 to 85 do
+          begin
+          for j:=1 to 170 do
+            begin
+            Image2.Canvas.pixels[i,j]:=Image1.canvas.pixels[i,j];
+            end;
+          end;
+      for i:=86 to 170 do
+        begin
+        for j:=1 to 170 do
+          begin
+            Image2.canvas.pixels[i,j]:=Image1.canvas.pixels[170-i,j];
+          end;
+        end;
+end;
+
+end.
+
